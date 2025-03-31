4c5ea5903a406c2c70b3fb8fe0a68cd692910222
MinhHieu <pmhieu2453@gmail.com>
2025-03-31 18:13:00 +0700

    ll

---
+++ Assets/Script/Traps/SpikeTraps.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SpikeTraps : MonoBehaviour
+{
+    [SerializeField] private int trapDamage; 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Kiểm tra nếu va chạm với Player
+        PlayerController player = collision.GetComponent<PlayerController>();   
+        if (player != null)
+        {
+            player.TakeDamage(trapDamage); // Gọi hàm TakeDamage từ PlayerController
+        }
+    }
+}
